ff16857ba124be645f68c19aa21684bfd48a92fb
anthonysalas11 <anthony.salas.ts@gmail.com>
2025-12-13 21:09:10 -0500

    complete

--- src/bad-hoist.js
+++ src/bad-hoist.js
@@ -1,13 +1,13 @@
 const debugFunc = () => {
-  time = 'today';
+  const time = 'today';
   let name = 'Zo';
   let mood = 'happy';
 
-  console.log(`Hello ${name} , are you feeling ${mood} ${time}  ?`);
+  console.log(`Hello ${name}, are you feeling ${mood} ${time}?`);
 
 
   mood = 'sad'
-  console.log("Oh no, I'm sorry you're feeling " + mood + ' ' + time + '.');
+  console.log (`Oh no, I'm sorry you're feeling ${mood} ${time}.`);
   ;
 };
 
